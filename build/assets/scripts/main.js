60b4fe69b959dad25a5c6c64c0666e1a673518c1
Denis Davydov <den.saint.k@gmail.com>
2018-11-20 10:37:43 +0300

    add adaptive menu

--- build/assets/scripts/main.js
+++ build/assets/scripts/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
 
+  $('.header__menu').on('click', function () {
+    $('.services--menu').toggleClass('services--menu--active');
+    $('.menu__text').toggleClass('menu__text--active');
+    $('.menu__close').toggleClass('menu__close--active');
+    $('.menu__img').toggleClass('menu__img--active');
+    $('.menu__plus').toggleClass('menu__plus--active');
+  });
+
   $('.hero__slider').slick({
     arrows: false
   });
