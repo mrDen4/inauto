de8d3aea64a5a00ce20959456b88bd01d170bb3f
Denis Davydov <den.saint.k@gmail.com>
2018-11-21 19:32:12 +0300

    add so much

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,16 @@ $(document).ready(function () {
     $('.menu__plus').toggleClass('menu__plus--active');
   });
 
+  $('.slide__btn, .contacts__btn, .service__btn').on('click', function () {
+    $('.popup').toggleClass('popup--active');
+    $('.bg').toggleClass('bg--active');
+  });
+
+  $('.bg, .popup__exit, .popup .popup__form .form__row .row__btn').on('click', function () {
+    $('.popup').toggleClass('popup--active');
+    $('.bg').toggleClass('bg--active');
+  });
+
   $('.hero__slider').slick({
     arrows: false
   });
